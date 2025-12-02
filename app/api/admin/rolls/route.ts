e300febd4f4cd6b475cb01a4cba9c4712fd07eb8
nagesh patidar <nageshpatidar021999@gmail.com>
2025-12-02 18:37:13 +0530

    modify

--- app/api/admin/rolls/route.ts
+++ app/api/admin/rolls/route.ts
@@ -1,30 +1,4 @@
-// import { NextResponse } from "next/server";
-// import { connectDB } from "@/app/lib/db";
-// import Roll from "@/app/models/Roll";
 
-// export async function GET() {
-//   try {
-//     await connectDB();
-//     const rolls = await Roll.find().sort({ createdAt: -1 });
-//     return NextResponse.json(rolls, { status: 200 });
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ message: "Server error" }, { status: 500 });
-//   }
-// }
-
-// export async function POST(req: Request) {
-//   try {
-//     await connectDB();
-//     const { number, reward, code, instantId } = await req.json();
-//     // Save all required fields
-//     const roll = await Roll.create({ number, reward, code, instantId });
-//     return NextResponse.json({ roll }, { status: 201 });
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ message: "Server error" }, { status: 500 });
-//   }
-// }
 
 import { NextResponse } from "next/server";
 import { connectDB } from "@/app/lib/db";
